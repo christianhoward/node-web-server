879daa382b998e8674d631a740a4700c8a4162d9
christianhoward <christian.john.howard@gmail.com>
2017-02-08 22:36:30 -0500

    Added a projects page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    projectMessage: 'Portfolio page here.'
+  });
+});
+
 // /bad send back JSON with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
